fc6b50a497ba35bd8c1c668608423b9a851ed75f
Chris Pardy <cooliomcdude@gmail.com>
2015-12-11 19:54:26 +1100

    works and is faster

--- R/mmik.R
+++ R/mmik.R
@@ -67,3 +67,41 @@ mmik <-function(cts, disc, bw=rep(0,length(unique(disc))),skewnessCorrection=TRU
 
 library(Rcpp)
 sourceCpp("mmik.cpp")
+
+mmikc <-function(cts, disc, bw=rep(0,length(unique(disc))),skewnessCorrection=TRUE){
+  
+  N <- length(disc)
+  
+  if (N !=length(cts)){
+    stop("disc and cts must have the same lengths")
+  }
+  disc<-factor(disc)
+  
+  sortOrder <- order(cts)
+  disc <-disc[sortOrder]
+  cts <-cts[sortOrder]
+  
+  cts <- cts-min(cts)+0.5
+
+# The fllowing function uses codes from   
+#  http://rstudio-pubs-static.s3.amazonaws.com/1563_1ae2544c0e324b9bb7f6e63cf8f9e098.html
+
+  if(skewnessCorrection & abs(skewness(cts)) > 0.5){
+    skew.score <- function(c, x) (skewness(log(x + c)))^2
+    best.c <- optimise(skew.score, c(0, 20), x = cts)$minimum
+    cts <- log(cts + best.c)
+  }
+  
+  m <- length(levels(disc))
+  Ni <- table(disc)
+  
+  ctslevels <- split(cts, disc)
+
+  if(bw[1]==0){
+    for(i in 1:m){
+      bw[i]<-sd(ctslevels[[i]])
+    }
+  }
+  
+  return(mmikCpp(ctslevels, bw, N, cts, Ni)$MI)
+}
--- R/mmik.cpp
+++ R/mmik.cpp
@@ -3,62 +3,75 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 List mmikCpp(List ctsLevels, NumericVector bws, 
-		NumericVector N, NumericVector cts) 
+        NumericVector N, NumericVector cts, NumericVector Ni) 
 {
 
-	int levels = ctsLevels.size();
-	int n = N[0];
+    int levels = ctsLevels.size();
+    int n = N[0];
 
-	NumericVector outvec(levels);
+    // Accumulators
+    IntegerVector k(n); //make first two integers?
+    IntegerVector ka(n);
+    NumericVector d(n);
 
-	// Accumulators
-	NumericVector k(n); //make first two integers?
-	NumericVector ka(n);
-	NumericVector d(n);
+    int kk = 0;
+    for (int i = 0; i < levels; i++)
+    {
+        SEXP ll = ctsLevels[i];
+        NumericVector y(ll);
 
-	int kk = 0;
-	for (int i = 0; i < levels; i++)
-	{
-		SEXP ll = ctsLevels[i];
-		NumericVector y(ll);
-		outvec[i] = sum(y);
-		
-		for (int j = 0; j < y.size(); j++)
-		{
-			for (int l = 0; l < y.size(); l++)
-			{
-				if (std::abs(y[l] - y[j]) < bws[i])
-				{
-					k[kk] += 1; // Number of points within BW
+        for (int j = 0; j < y.size(); j++)
+        {
+            for (int l = 0; l < y.size(); l++)
+            {
+                if (std::abs(y[l] - y[j]) < bws[i])
+                {
+                    k[kk] += 1; // Number of points within BW
 
-					// Max distance to point within BW
-					if (d[kk] < std::abs(y[l] - y[j]))
-					{
-						d[kk] = std::abs(y[l] - y[j]);
-					}
-				}
+                    // Max distance to point within BW
+                    if (d[kk] < std::abs(y[l] - y[j]))
+                    {
+                        d[kk] = std::abs(y[l] - y[j]);
+                    }
+                }
 
-			}
+            }
 
-			k[kk] = std::max(k[kk] - 1.0, 1.0);
-			// This is why I should make the vectors integer
+            k[kk] = std::max(k[kk] - 1, 1);
+            // This is why I should make the vectors integer
 
-			// Some redundancy here
-			for (int l = 0; l < cts.size(); l++)
-			{
-				if (std::abs(cts[l] - y[j]) < d[kk])
-				{
-					ka[kk] += 1;
-				}
-			}
-		
-			
-			kk += 1;
-		}
+            if (d[kk] == 0 | NumericVector::is_na(d[kk]))
+            {
+                if (j == 0) d[kk] = std::abs(y[j] - y[j + 1]);
+                else if (j == y.size() - 1) d[kk] = std::abs(y[j] - y[j - 1]); 
+                else
+                {
+                    d[kk] = std::max(std::abs(y[j] - y[j + 1]), 
+                            std::abs(y[j] - y[j - 1]));
+                }
 
+            }
 
-	}
+            // Count points in all groups closer than d[kk]
+            // Some redundancy here
+            for (int l = 0; l < cts.size(); l++)
+            {
+                if (std::abs(cts[l] - y[j]) < d[kk])
+                {
+                    ka[kk] += 1;
+                }
+            }
 
-	//outvec[0] = sum(bws);
-	return List::create(Named("outvec") = outvec, Named("k") = k, Named("d") = d, Named("ka") = ka);
+            kk += 1;
+        }
+    }
+
+    NumericVector MI(1);
+    MI = mean(digamma(k) - digamma(ka)) 
+        + sum(Ni * (-digamma(Ni))) / N
+        + digamma(N);
+
+    //outvec[0] = sum(bws);
+    return List::create(Named("k") = k, Named("d") = d, 
+            Named("ka") = ka, Named("MI") = MI);
 }	 
