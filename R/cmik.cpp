6444ca1194d98f91281ad6c991842f93230441c4
Chris Pardy <cooliomcdude@gmail.com>
2015-12-22 18:09:10 +1100

    switch to column major (faster)

--- R/cmik.cpp
+++ R/cmik.cpp
@@ -11,12 +11,12 @@ NumericVector cmikCpp(NumericVector bws,
 
     // X differences
     NumericMatrix xdiffs(n);
-    for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
     {
-        for (int j = i; j < n; j++)
+        for (int i = j; i < n; i++)
         {
             if (j == i) continue; // Keep 0s on diagonal.
-            
+
             xdiffs(i, j) = std::abs(X[i] - X[j]);
             xdiffs(j, i) = xdiffs(i, j); // Symmetry of abs().
         }
@@ -24,12 +24,12 @@ NumericVector cmikCpp(NumericVector bws,
 
     // Y differences
     NumericMatrix ydiffs(n);
-    for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
     {
-        for (int j = i; j < n; j++)
+        for (int i = j; i < n; i++)
         {
             if (j == i) continue; // Keep 0s on diagonal.
-            
+
             ydiffs(i, j) = std::abs(Y[i] - Y[j]);
             ydiffs(j, i) = ydiffs(i, j); // Symmetry of abs().
         }
@@ -37,25 +37,16 @@ NumericVector cmikCpp(NumericVector bws,
 
     // Max norm distances
     NumericMatrix distmat(n);
-    for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
     {
-        for (int j = i; j < n; j++)
+        for (int i = j; i < n; i++)
         {
             if (j == i) continue; // Keep 0s on diagonal.
-            
+
             distmat(i, j) = std::max(xdiffs(i, j), ydiffs(i, j));
             distmat(j, i) = distmat(i, j); // Symmetry.
         }
     }
-
-    // Don't need all these to be vectors, keep for the moment
-    // to check intermediate results.
-    //
-    // NumericVector s(n);
-    // NumericVector sN(n);
-    // NumericVector t(n);
-    // NumericVector tN(n);
-    // NumericVector eD(n); 
 
     int s = 0;
     int sN = 0;
@@ -63,12 +54,10 @@ NumericVector cmikCpp(NumericVector bws,
     int tN = 0;
     double eD = 0; 
 
-    // need k, l and m.
+    // Need to keep k, l and m for all i.
     NumericVector k(n); 
     NumericVector l(n);
     NumericVector m(n);
-
-    // NumericVector dists(2);
 
     // Finding furthest point within bw.
     int sN_inbw_index = 0;
@@ -92,6 +81,7 @@ NumericVector cmikCpp(NumericVector bws,
     int kx = 0;
     int ky = 0;
 
+    // Main loop
     for (int i = 0; i < n; i++)
     {
         s = 0;
@@ -100,19 +90,10 @@ NumericVector cmikCpp(NumericVector bws,
         tN = 0;
         eD = 0; 
 
-        // N.b. C++ std:sort is faster than R
-        //
-        // R matrices are row-major. Need to check
-        // if RCpp NumericMatrix is too.
-        NumericVector xdi = xdiffs(i, _);
-        NumericVector ydi = ydiffs(i, _);
-        NumericVector d = distmat(i, _);
-
-        // Sorting breaks connection between X & Y.
-        // May need to fins another approach.
-        // std::sort(xdi.begin(), xdi.end());
-        // std::sort(ydi.begin(), ydi.end());
-        // std::sort(d.begin(), d.end());
+        // N.b. R matrices are column-major.
+        NumericVector xdi = xdiffs(_, i);
+        NumericVector ydi = ydiffs(_, i);
+        NumericVector d = distmat(_, i);
  
         // get s(i) and sN(i)
         sN_inbw_index = 0;
@@ -162,6 +143,7 @@ NumericVector cmikCpp(NumericVector bws,
                 }
             }
          }
+        // Must have s > 1
         if (s > 1)
         {
             sN = sN_inbw_index;
@@ -171,12 +153,6 @@ NumericVector cmikCpp(NumericVector bws,
             s = 2;
             sN = sN_outbw_index;
         }
-
-        // get t(i)
-//        for (int j = 0; j < n; j++)
-//        {
-//            if (ydi[j] < bws[1]) t += 1; 
-//        }
 
         // get t(i) snd tN(i)
         tN_inbw_index = 0;
@@ -198,7 +174,7 @@ NumericVector cmikCpp(NumericVector bws,
                     tN_inbw_value = ydi[j];
                     tN_inbw_candidate_found = true;
                 }
-                else if (ydi[j] > tN_inbw_value) // N.b. greater than
+                else if (ydi[j] > tN_inbw_value) // N.b. greater thanR 
                 {
                     tN_inbw_index = j;
                     tN_inbw_value = ydi[j];
@@ -207,8 +183,8 @@ NumericVector cmikCpp(NumericVector bws,
             else
             {
                 // Find closest point outside bw.
-                // Used in case where s == 1, and
-                // we need to set it to s == 2.
+                // Used in case where t == 1, and
+                // we need to set it to t == 2.
                 if(!tN_outbw_candidate_found)
                 {
                     tN_outbw_index = j;
@@ -222,6 +198,7 @@ NumericVector cmikCpp(NumericVector bws,
                 }
             }
          }
+        // Must have t > 1 
         if (t > 1)
         {
             tN = tN_inbw_index;
@@ -248,8 +225,9 @@ NumericVector cmikCpp(NumericVector bws,
                 ky += 1;
             }
         }
-        // Don't implement kmax for now.
 
+        // Limit k to kmax. If this happens we then need
+        // to find the corresponding distance from d.
         if (km < kx && km < ky)
         {
             k[i] = km;
@@ -259,6 +237,7 @@ NumericVector cmikCpp(NumericVector bws,
         }
         else
         {
+            // If k <= kmax we already have the correct distance.
             if (kx <= ky)
             {
                 k[i] = kx;
@@ -290,20 +269,5 @@ NumericVector cmikCpp(NumericVector bws,
 
     MI = digamma(N) + mean(digamma(k)) - mean(digamma(l) + digamma(m));
 
-//    List ret;
-//    ret["xdiffs"] = xdiffs;
-//    ret["ydiffs"] = ydiffs;
-//    ret["distmat"] = distmat;
-//    ret["s"] = s;
-//    ret["t"] = t;
-//    ret["sN"] = sN; // values rather than indices.
-//    ret["tN"] = tN;
-//    //ret["xeD"] = xeD;
-//    //ret["yeD"] = yeD;
-//    ret["k"] = k;
-//    ret["l"] = l;
-//    ret["m"] = m;
-//    ret["eD"] = eD;
-//    ret["MI"] = MI;
     return(MI);
 } 
